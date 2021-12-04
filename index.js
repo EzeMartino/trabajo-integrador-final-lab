ccb28f3e4042ad82f9b4bebf0a3ca51138ecde2b
EzeMartino <ezequielrmartino@gmail.com>
2021-12-03 21:18:02 -0300

    funciones básicas

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 boton = document.querySelector("button")
 
 ciudad = document.querySelector("select")
+
+resultado = document.getElementById("resultado")
+
+APPID = "3936d0749fdc3124c6566ed26cf11978" /* ID de la app weather que pasaron los profes */
+
+function error(){
+    resultado.style.backgroundColor="red"
+    resultado.innerText= "Error: Elija una de las ciudades"
+}
+
+function verClima(){
+    climaciudad = ciudad.value
+    if(climaciudad == "0"){
+        error()
+    } else {
+        alert("La ciudad elegida es " + climaciudad)
+    }
+    
+}
