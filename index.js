f906eac806c5bf7bff4a0730a776f6d04956a06e
Tomás Sacripanti <sacripantitomas@gmail.com>
2021-12-07 16:19:23 -0300

    Maquetado básico + Lógica terminada

--- index.js
+++ index.js
@@ -1,22 +1,85 @@
-boton = document.querySelector("button")
+//Definición de constantes
+const API_KEY = "3936d0749fdc3124c6566ed26cf11978";
+const CITIES_SELECT = document.querySelector(".cities");
 
-ciudad = document.querySelector("select")
 
-resultado = document.getElementById("resultado")
+//En caso de no estar creado el arreglo de ciudades en localStorage = crearlo
+if (localStorage.getItem("cities") === null) {
+    localStorage.setItem("cities", JSON.stringify([]));
+};
 
-APPID = "3936d0749fdc3124c6566ed26cf11978" /* ID de la app weather que pasaron los profes */
 
-function error(){
-    resultado.style.backgroundColor="red"
-    resultado.innerText= "Error: Elija una de las ciudades"
+//En caso de haber ciudades en localStorage agregarlas al select cuando cargue el DOM
+window.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+    if (localStorage.getItem("cities") !== null) {
+        loadOptions(loadCitiesFromStorage());
+    } 
+});
+
+
+//Función ejecutada cuando un usuario agrega una ciudad en la página "Agregar ciudad"
+function addCityToStorage(element) {
+    let city = element.parentElement.querySelector("#city").value;
+    let cities = loadCitiesFromStorage();
+    cities.push(city);
+    localStorage.setItem("cities", JSON.stringify(cities));
+    loadOptions(cities);
 }
 
-function verClima(){
-    climaciudad = ciudad.value
-    if(climaciudad == "0"){
-        error()
-    } else {
-        alert("La ciudad elegida es " + climaciudad)
+
+//Devuelve el arreglo de ciudades proveniente del localStorage
+function loadCitiesFromStorage() {
+    let cities = JSON.parse(localStorage.getItem("cities"));
+    return cities
+}
+
+
+//Carga un arreglo de options dentro del Select de ciudades
+function loadOptions(cities) {
+    cities.forEach(city => {
+        CITIES_SELECT.innerHTML += `<option value="${city}">${city}</option>`;
+    });
+}
+
+
+//Función que trae los datos del clima de la API y devuelve un string 
+//que se corresponde con la respuesta del servidor para posteriormente
+//pasarlo a la función showMessage
+async function fetchWeather(element){
+    try {
+        let city = element.parentElement.querySelector(".cities").value;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=es`;
+        console.log(url);
+        let response = await fetch(url);
+        let data = await response.json();
+        let {temp, pressure, humidity, feels_like} = data.main;
+        let { description, icon} = data.weather[0];
+        createMessage({temp, pressure, humidity, feels_like, description, icon});
+    } catch (error) {
+        showMessage(error.message, true);
     }
-    
 }
+
+
+//Función que crea el mensaje a mostrar en caso de obtener una respuesta exitosa
+function createMessage(weatherObject) {
+    console.log(weatherObject);
+
+
+
+    showMessage(message, false);
+} 
+
+//Función que muestra el resultado o el error dependiendo los parámetros
+function showMessage(message, error) {
+    // if (error === true) {
+    //     resultado.style.backgroundColor="red";
+    //     resultado.innerText = message;
+    // } else {
+    //     resultado.style.backgroundColor="green";
+    //     resultado.innerText = message;
+    // }
+}
+
+
